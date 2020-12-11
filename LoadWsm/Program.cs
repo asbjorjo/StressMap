07c0c04069293ab942998d8d3b309c86ec9c2a87
Asbjoern L. Johansen <asbjorjo@gmail.com>
2020-12-11 21:07:20 +0100

    Pull code to read and post data from WSM CSV file to functions.

--- LoadWsm/Program.cs
+++ LoadWsm/Program.cs
@@ -15,12 +15,74 @@ namespace LoadWsm
 {
     class Program
     {
-        static async System.Threading.Tasks.Task Main(string[] args)
+        static async Task Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+                
+            const string fileName = "C:\\Users\\asbjo\\Downloads\\wsm2016.csv";
+            const string ApiUri = "http://stressapi-dev.azurewebsites.net/";
+            const int maxPostRequests = 1000;
 
-            var fileName = "C:\\Users\\asbjo\\Downloads\\wsm2016.csv";
+            await LoadWsmCsv(fileName, ApiUri, maxPostRequests);
+        }
 
+        private static async Task LoadWsmCsv(string fileName, string apiUri, int maxPostRequests)
+        {
+            var handler = new HttpClientHandler
+            {
+                MaxConnectionsPerServer = 5
+            };
+            List<StressRecord> records = ReadWsmCsv(fileName);
+            Console.WriteLine(records.Count);
+
+            await SendWsmData(apiUri, maxPostRequests, records, handler);
+
+            using (var client = new HttpClient(handler))
+            {
+                client.BaseAddress = new Uri(apiUri);
+
+                var responseTask = client.GetAsync("StressRecord");
+
+                Console.WriteLine(responseTask.Result);
+                var fromapi = JsonSerializer.Deserialize<List<StressRecord>>(await responseTask.Result.Content.ReadAsStringAsync());
+
+                Console.WriteLine(fromapi.Count);
+            }
+        }
+
+        private static async Task SendWsmData(string ApiUri, int maxPostRequests, List<StressRecord> records, HttpClientHandler handler)
+        {
+            using (var client = new HttpClient(handler))
+            {
+                //client.BaseAddress = new Uri(ApiUri);
+                client.BaseAddress = new Uri(ApiUri);
+
+                var jsonOptions = new JsonSerializerOptions();
+                jsonOptions.Converters.Add(new JsonStringEnumConverter());
+                jsonOptions.Converters.Add(new NetTopologySuite.IO.Converters.GeoJsonConverterFactory());
+
+                var tasks = new List<Task>();
+                var i = 0;
+                foreach (var stressrecord in records)
+                {
+                    var dataString = JsonSerializer.Serialize(stressrecord, jsonOptions);
+                    var httpContent = new StringContent(dataString);
+                    httpContent.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
+                    tasks.Add(client.PostAsync("StressRecord", httpContent));
+                    i++;
+
+                    if (i % maxPostRequests == 0)
+                    {
+                        Console.WriteLine(i);
+                        await Task.WhenAll(tasks);
+                        tasks.Clear();
+                    }
+                }
+            }
+        }
+
+        private static List<StressRecord> ReadWsmCsv(string fileName)
+        {
             var records = new List<StressRecord>();
 
             using (var reader = new StreamReader(fileName))
@@ -28,7 +90,7 @@ namespace LoadWsm
             {
                 csv.Read();
                 csv.ReadHeader();
-
+    
                 while (csv.Read())
                 {
                     var record = new StressRecord
@@ -51,54 +113,13 @@ namespace LoadWsm
                     records.Add(record);
                 }
 
-                var recordEnum = records.GetEnumerator();
+                //var recordEnum = records.GetEnumerator();
 
-                recordEnum.MoveNext();
-                Console.WriteLine(recordEnum.Current);
+                //recordEnum.MoveNext();
+                //Console.WriteLine(recordEnum.Current);
             }
 
-            Console.WriteLine(records.Count);
-
-            var handler = new HttpClientHandler
-            {
-                MaxConnectionsPerServer = 5
-            };
-
-            using (var client = new HttpClient(handler))
-            {
-                //client.BaseAddress = new Uri("https://localhost:5001/StressRecord/");
-                client.BaseAddress = new Uri("http://stressmap-dev.azurewebsites.net/");
-
-                var jsonOptions = new JsonSerializerOptions();
-                jsonOptions.Converters.Add(new JsonStringEnumConverter());
-                jsonOptions.Converters.Add(new NetTopologySuite.IO.Converters.GeoJsonConverterFactory());
-
-                var tasks = new List<Task>();
-                var i = 0;
-                foreach (var stressrecord in records)
-                {
-                    i++;
-                    var dataString = JsonSerializer.Serialize(stressrecord, jsonOptions);
-                    var httpContent = new StringContent(dataString);
-                    httpContent.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
-                    tasks.Add(client.PostAsync("StressRecord", httpContent));
-
-                    if (i % 1000 == 0)
-                    {
-                        Console.WriteLine(i);
-                        await Task.WhenAll(tasks);
-                        tasks.Clear();
-                    }
-                }
-
-
-                var responseTask = client.GetAsync("StressRecord");
-
-                Console.WriteLine(responseTask.Result);
-                var fromapi = JsonSerializer.Deserialize<List<StressRecord>>(await responseTask.Result.Content.ReadAsStringAsync());
-
-                Console.WriteLine(fromapi.Count);
-            }
+            return records;
         }
     }
 }
