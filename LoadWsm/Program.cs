5a49cda8f25fe2cb398e93f434ee8b19625dbad5
Asbjoern L. Johansen <asbjorjo@gmail.com>
2020-12-15 19:47:31 +0100

    Add code to load plates.

--- LoadWsm/Program.cs
+++ LoadWsm/Program.cs
@@ -1,5 +1,6 @@
 ﻿using CsvHelper;
 using NetTopologySuite.Geometries;
+using StressData.Database.Constants;
 using StressData.Model;
 using System;
 using System.Collections.Generic;
@@ -15,27 +16,122 @@ namespace LoadWsm
 {
     class Program
     {
+        private const int maxConnections = 5;
+        private const int maxPostRequests = 1000;
+
         static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-                
-            const string fileName = "C:\\Users\\asbjo\\Downloads\\wsm2016.csv";
-            const string ApiUri = "http://stressapi-dev.azurewebsites.net/";
-            const int maxPostRequests = 1000;
+            bool loadWsm = false;
+            bool loadPlates = true;
+            
+            const string fileNameWsmCsv = "C:\\Users\\asbjo\\Downloads\\wsm2016.csv";
+            const string fileNamePlates = "C:\\Users\\asbjo\\Downloads\\PB2002_plates.dig.txt";
+            //const string ApiUri = "http://stressapi-dev.azurewebsites.net/";
+            const string ApiUri = "https://localhost:5001/";
+            
+            if (loadWsm) 
+            {
+                await LoadWsmCsv(fileNameWsmCsv, ApiUri);
+            }
 
-            await LoadWsmCsv(fileName, ApiUri, maxPostRequests);
+            if (loadPlates)
+            {
+                await LoadPlatesAsync(fileNamePlates, ApiUri);                
+            }
         }
 
-        private static async Task LoadWsmCsv(string fileName, string apiUri, int maxPostRequests)
+        private static async Task LoadPlatesAsync(string fileNamePlates, string apiUri)
+        {
+            List<StressPlate> plates = new List<StressPlate>();
+            var geometryFactory = new GeometryFactory(new PrecisionModel(), GeometryConstants.SRID);
+
+            using (var reader = new StreamReader(fileNamePlates))
+            {
+                var name = reader.ReadLine();
+                List<Coordinate> coordinates = new List<Coordinate>();
+
+                while (reader.Peek() >= 0)
+                {
+                    var line = reader.ReadLine();
+                    if (line != "*** end of line segment ***")
+                    {
+                        var split = line.Split(',');
+                        var format = CultureInfo.CreateSpecificCulture("en-US");
+                        var lon = double.Parse(split[0], NumberStyles.Float, format);
+                        var lat = double.Parse(split[1], NumberStyles.Float, format);
+                        var coordinate = new Coordinate(lon , lat);
+                        coordinates.Add(coordinate);
+                    }
+                    else
+                    {
+                        var plate = new StressPlate
+                        {
+                            Name = name,
+                            Outline = new Polygon(new LinearRing(coordinates.ToArray()), geometryFactory)
+                        };
+                        
+                        plate.Outline.SRID = GeometryConstants.SRID;
+
+                        plates.Add(plate);
+                        name = reader.ReadLine();
+                        coordinates = new List<Coordinate>();
+                    }
+                }
+            }
+
+            var handler = new HttpClientHandler
+            {
+                MaxConnectionsPerServer = maxConnections
+            };
+
+            using (var client = new HttpClient(handler))
+            {
+                client.BaseAddress = new Uri(apiUri);
+
+                var jsonOptions = new JsonSerializerOptions();
+                jsonOptions.Converters.Add(new JsonStringEnumConverter());
+                jsonOptions.Converters.Add(new NetTopologySuite.IO.Converters.GeoJsonConverterFactory());
+                var tasks = new List<Task>();
+                var i = 0;
+                
+                foreach (var plate in plates)
+                {
+                    var dataString = JsonSerializer.Serialize(plate, jsonOptions);
+                    var httpContent = new StringContent(dataString);
+                    httpContent.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
+
+                    //var response = await client.PostAsync("StressPlate", httpContent);
+                    //Console.WriteLine(response.StatusCode);
+                    //Console.WriteLine(await response.Content.ReadAsStringAsync());
+
+                    tasks.Add(client.PostAsync("StressPlate", httpContent));
+                    i++;
+
+                    if (i % maxPostRequests == 0)
+                    {
+                        Console.WriteLine(i);
+                        await Task.WhenAll(tasks);
+                        tasks.Clear();
+                    }
+                }
+                if (tasks.Count > 0)
+                {
+                    await Task.WhenAll(tasks);
+                    tasks.Clear();
+                }
+            }
+        }
+
+        private static async Task LoadWsmCsv(string fileName, string apiUri)
         {
             var handler = new HttpClientHandler
             {
-                MaxConnectionsPerServer = 5
+                MaxConnectionsPerServer = maxConnections
             };
             List<StressRecord> records = ReadWsmCsv(fileName);
             Console.WriteLine(records.Count);
 
-            await SendWsmData(apiUri, maxPostRequests, records, handler);
+            await SendWsmData(apiUri, maxPostRequests, records);
 
             using (var client = new HttpClient(handler))
             {
@@ -50,8 +146,13 @@ namespace LoadWsm
             }
         }
 
-        private static async Task SendWsmData(string ApiUri, int maxPostRequests, List<StressRecord> records, HttpClientHandler handler)
+        private static async Task SendWsmData(string ApiUri, int maxPostRequests, List<StressRecord> records)
         {
+            var handler = new HttpClientHandler
+            {
+                MaxConnectionsPerServer = maxConnections
+            };
+            
             using (var client = new HttpClient(handler))
             {
                 //client.BaseAddress = new Uri(ApiUri);
@@ -77,6 +178,11 @@ namespace LoadWsm
                         await Task.WhenAll(tasks);
                         tasks.Clear();
                     }
+                }
+                if (tasks.Count > 0)
+                {
+                    await Task.WhenAll(tasks);
+                    tasks.Clear();
                 }
             }
         }
